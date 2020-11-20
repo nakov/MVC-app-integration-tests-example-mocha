1043feb7236ca07cc19beb058c3148bf3787e06e
Svetlin Nakov <github@nakov.com>
2020-11-20 23:04:42 +0200

    Fixed app port to use `env.PORT`

--- index.js
+++ index.js
@@ -11,11 +11,16 @@ let students = require("./models/students-model");
 
 studentsController.setup(app, students);
 
-app.listen(8080)
+let port = process.argv[2];
+if (!port) port = process.env['PORT'];
+if (!port) port = 8080;
+
+app.listen(port, () => {
+  console.log(`App started. Listening at http://localhost:${port}`);
+})
 .on('error', function(err) {
   if (err.errno === 'EADDRINUSE')
-     console.error("Port 8080 busy (server already started).");
+    console.error(`Port ${port} busy.`);
   else 
     throw err;
 });
-
