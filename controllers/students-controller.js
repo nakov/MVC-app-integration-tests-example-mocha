eabffbcc39320f12cdefa4a3b325f5108e9f61c7
Svetlin Nakov <nakov@users.noreply.github.com>
2021-07-31 15:32:18 +0300

    Update students-controller.js

--- controllers/students-controller.js
+++ controllers/students-controller.js
@@ -1,7 +1,7 @@
 function setup(app, students) {
   app.get('/', function(req, res) {
     let model = {
-      title: "MVC Example",
+      title: "MVC Example - new",
       msg: "Students Registry",
       students: students
     };
